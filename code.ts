2747aec3d555113785dbce80e811957055756523
godo13405 <goncaloandraderdovale@gmail.com>
2026-01-21 14:53:50 +0000

    csv report

--- code.ts
+++ code.ts
@@ -213,9 +213,11 @@ const setTitle = ({ state }) => {
 
 const getLink = (node) => {
 	const { id } = node;
-	return `https://www.figma.com/design/${
+	const output = `https://www.figma.com/design/${
 		figma.fileKey
-	}/${figma.root.name.replace(" ", "-")}?node-id=${id.replace(":", "-")}`;
+	}/${figma.root.name.toLowerCase().replace(" ", "-")}?node-id=${id.replace(":", "-")}`;
+
+	return output;
 };
 
 const writeLine = async ({ node, isFirst, container }) => {
@@ -403,7 +405,6 @@ const runReport = async () => {
 		reportLastModified.opacity = 0.6;
 		headerContainer.appendChild(reportLastModified);
 	}
-
 
 	// find all sections
 	const NAME_REGEX = /^\{.*?\}\s\[[A-Z]{2}\]/;
@@ -733,6 +734,34 @@ const figmaCommand = (command) => {
 	}
 };
 
+const exportTo = ({ type = "csv" }) => {
+	let csvString = "Section Name,Status,Author,Date Modified,Link\n";
+
+	// find all sections
+	const NAME_REGEX = /^\{.*?\}\s\[[A-Z]{2}\]/;
+	const sections = figma.currentPage.findChildren((node) => {
+		return node.type === "SECTION" && NAME_REGEX.test(node.name);
+	});
+
+	sections.forEach((section) => {
+		section.name = section.name.trim();
+
+		const link = getLink(section)
+		const name = section.name.match(/] ([\d\D]*)$/gm)![0].substring(2).trim();
+		const emoji = section.name.match(/^\{([^\}]*)/gm)![0].substring(1).trim().split(" ")[0];
+		const status = section.name.match(/^\{([^\}]*)/gm)![0].substring(3).trim();
+		
+		csvString += `${name}, ${emoji} ${status}, ${section.getPluginData("authorFullName")}, ${section.getPluginData("dateModified")}, ${link}\n`;
+	});
+
+	figma.ui.postMessage({
+		type: "export",
+		options: csvString
+	});
+
+	figma.notify(`Report exported`);
+}
+
 const options = getOptions();
 
 figma.ui.onmessage = ({
@@ -768,6 +797,9 @@ figma.ui.onmessage = ({
 			setOptions(options);
 			options = getOptions();
 			figma.notify(`Report option set`);
+			break;
+		case "exportTo":
+			exportTo({type: 'csv'});
 			break;
 		default:
 			setTitle({ state });
